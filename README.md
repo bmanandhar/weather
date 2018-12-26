a66023aeac10f336df1bd18a97988600e1026b91
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-12-25 20:37:37 -0800

    ready go

---
+++ README.md
@@ -0,0 +1,26 @@
+## Project name: Weather Report
+
+This project has been developed as a part of learning how to develope website using the some of the technologies that I know. The web app has been designed to render the weather report of any city that has the record with 'Open Weather Map' `https://openweathermap.org/`. I was able to access the API data by achieving an API key.
+
+### User story
+
+* User can access the crrent weather data of almost any city of the world by entering the city name. 
+* It will display an error message if the city name entered is incorrect.
+* Currently 10 different types of data are made available including an image-icon that reflects sky condition of the city.
+
+### Chalenges
+
+* Writing jQuery function. 
+* Function AJAX get method using the url.
+
+### Future feature
+
+This web app may be made to show data in different measurement system such as Imperial, Metric, and Absolute. Hence by assigning one of the systems as default it can show the data in other systems by using button/s and jQuery fuction/s with toggle event function.
+
+### Technologies used
+
+* HTML
+* CSS and Bootstrap library
+* javaScript and jQuery library
+* Weather data in json format
+* AJAX GET method request
---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#submit").click(function(){
+        var city = $("#city").val();
+        if (city != ""){
+            $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+"&appid=8f236e524e673e139b0f6739b81a6eb4",
+            type: "GET",
+            dataType: "json",
+            success:function(data){
+                console.log(data);
+                var widget= show(data);
+                $("#show").html(widget);
+            }
+        });
+        } else {
+            $("#error").html("City name can not be empty")
+        }
+    });
+});
+function show(data){
+    return (
+        "<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'/>"+
+        "<h4>Weather: "+data.weather[0].main+"</h5>"+
+        "<h4>Description: "+data.weather[0].description+"</h4>"+
+        "<h4>Temperature: "+data.main.temp+"&#8457</h4>"+
+        "<h4>Pressure: "+data.main.pressure+"hPa</h4>"+
+        "<h4>Humidity: "+data.main.humidity+"%</h4>"+
+        "<h4>Temp Max: "+data.main.temp_max+"&#8457</h4>"+
+        "<h4>Temp Min: "+data.main.temp_min+"&#8457</h4>"+
+        "<h4>Wind speed: "+data.wind.speed+"mile/hr</h4>"+
+        "<h4>Wind direction: "+data.wind.deg+"&#176</h4>"+
+        "<h4>[City: "+data.name+", "+data.sys.country+"]"+"<h4>"
+        );
+    }
+
