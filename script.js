7a0e768450d8ffbff755641d5bf166b659f3b6a3
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-12-30 16:22:19 -0800

    adds nothing

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
         var city = $("#city").val();
         if (city != ""){
             $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial"+"&appid=8f236e524e673e139b0f6739b81a6eb4",
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${city}
+                  &units=imperial&appid=8f236e524e673e139b0f6739b81a6eb4`,
             type: "get",
             dataType: "json",
             success:function(data){
